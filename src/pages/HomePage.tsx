d582b183bddb42518293c3da22a7eca94f8a5ea7
heric-hh <erhh.015@gmail.com>
2025-02-05 09:27:35 -0600

    06 useAppStore and services

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,4 @@
-import { useAppStore } from "../stores/useAppStore";
-
 export default function HomePage() {
-  useAppStore((state) => state.categories);
   return (
     <>
       <h1>Home Page</h1>
